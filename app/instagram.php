a8be7495309153e0abb4077ad75c9e450a01b9ff
drewmcd621 <mcdermotta@carnegiemuseums.org>
2015-09-24 14:36:50 -0400

    IG embeed

--- app/instagram.php
+++ app/instagram.php
@@ -36,7 +36,7 @@ function getTopIGMedia($userID, $count=10)
 
 function igEmbed($mediaurl)
 {
-  $data = array("html" => "<style>.embed-container {position: relative; padding-bottom: 120%; height: 0; overflow: hidden;} .embed-container iframe, .embed-container object, .embed-container embed { position: absolute; top: 0; left: 0; width: 100%; height: 100%; }</style><div class='embed-container'><iframe src='$mediaurlembed/' frameborder='0' scrolling='no' allowtransparency='true'></iframe></div>");
+  $data = array("html" => "<style>.embed-container {position: relative; padding-bottom: 120%; height: 0; overflow: hidden;} .embed-container iframe, .embed-container object, .embed-container embed { position: absolute; top: 0; left: 0; width: 100%; height: 100%; }</style><div class='embed-container'><iframe src='" . $mediaurl . "embed/' frameborder='0' scrolling='no' allowtransparency='true'></iframe></div>");
 
 	$object = json_decode(json_encode($data), FALSE);
   return $object;
