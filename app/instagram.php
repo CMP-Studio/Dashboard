b9e3289ea58cce424bd28ce92e6a93fd0d0d6205
drewmcd621 <mcdermotta@carnegiemuseums.org>
2015-09-24 14:30:23 -0400

    IG embeed

--- app/instagram.php
+++ app/instagram.php
@@ -36,9 +36,7 @@ function getTopIGMedia($userID, $count=10)
 
 function igEmbed($mediaurl)
 {
-  $url = "http://api.instagram.com/publicapi/oembed/";
-  $params = array("url"=>$mediaurl);
-  $data = getAPI($url, $params);
+  $data = array("html" => "<style>.embed-container {position: relative; padding-bottom: 120%; height: 0; overflow: hidden;} .embed-container iframe, .embed-container object, .embed-container embed { position: absolute; top: 0; left: 0; width: 100%; height: 100%; }</style><div class='embed-container'><iframe src='$mediaurl/embed/' frameborder='0' scrolling='no' allowtransparency='true'></iframe></div>");
 
   return $data;
 }
