1a76ac02a4d14c5db10a12e95327d9a1638344d4
drewmcd621 <mcdermotta@carnegiemuseums.org>
2015-09-28 11:09:26 -0400

    Starting analytics gathering

---
+++ app/cron/socialAnalytics.php
@@ -0,0 +1,34 @@
+<?php
+//Utils
+require_once "../utils/sql.php";
+require_once "../utils/api.php";
+//Configs
+require_once "../config/igConfig.php";
+require_once "../config/fbConfig.php";
+require_once "../config/twitConfig.php";
+
+main();
+
+function main()
+{
+  var_dump(twitStats("30018600"));
+}
+
+
+function twitStats($id)
+{
+  $token = getTwitterToken();
+  $headers = array("Authorization: Bearer " . $token);
+  $params = array("user_id" => $id);
+  $url = "https://api.twitter.com/1.1/users/show.json";
+  $info = getAPI($url, $params, $headers);
+
+  return array("followers" => $info->followers_count);
+
+}
+
+
+
+
+
+ ?>
