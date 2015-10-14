068f544f007106c7414f5c341da2eb3ede065c61
drewmcd621 <mcdermotta@carnegiemuseums.org>
2015-10-14 11:51:17 -0400

    Adding SW attendance

---
+++ app/sw.php
@@ -0,0 +1,20 @@
+<?php
+function getAttendQuery($loc, $start, $end)
+{
+  $query = "cast(start_date as date) as AttendDate, cast(sum(t.quantity * i.admissions) as int) as Admissions
+ from transact t
+ left join items i on (i.department + i.category + i.item = t.department + t.category + t.item)
+ where t.salespoint LIKE $loc + '%'
+ and (start_date BETWEEN $start AND $end)
+ and i.admprconly = 0
+ group by cast(start_date as date)
+ order by cast(start_date as date)";
+}
+
+function parseResult($result)
+{
+  $xml = simplexml_load_string($result);
+  var_dump($xml);
+}
+
+ ?>
