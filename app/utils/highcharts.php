59b9f78eee9db729ff1382626c4b9a852695adf8
drewmcd621 <mcdermotta@carnegiemuseums.org>
2015-10-22 16:45:45 -0400

    Revert "Merge branch 'social-chart'"
    This reverts commit fe9c5d448dd0b6a3532684c9fb3a30ace0249cbd, reversing
    changes made to f507275e906df02303c25d6bb236aee0ac98b857.

--- app/utils/highcharts.php
+++ app/utils/highcharts.php
@@ -45,13 +45,7 @@ class Highchart
 		$newdata = array();
 		foreach($data as $d)
 		{
-			if(empty($d))
-			{
-				array_push($newdata, null);
-			}
-			else {
-				array_push($newdata, round(floatval($d),2));
-			}
+			array_push($newdata, round(floatval($d),2));
 		}
 
 		return $newdata;
