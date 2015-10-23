3462f5305377e21fb681f449a75511f6d7dac105
drewmcd621 <mcdermotta@carnegiemuseums.org>
2015-10-23 15:37:24 -0400

    Testing the DNC flag

--- app/utils/analytics.php
+++ app/utils/analytics.php
@@ -21,7 +21,7 @@ function getClient()
 /* Checks for previous authentication then runs AUth if needed  returns auth token*/
 function Authenticate($client)
 {
-
+	return null; //QA
 	if (isset($_SESSION['service_token']))
 	{
 		$token = $_SESSION['service_token'];
