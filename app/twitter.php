19fcabecd4ae4d87e43b1c42eeaa9cb3d6c57769
drewmcd621 <mcdermotta@carnegiemuseums.org>
2015-10-23 11:06:09 -0400

    Simulate HTTP error - fixing

--- app/twitter.php
+++ app/twitter.php
@@ -124,9 +124,9 @@ function getTweetsByDate($user, $start=0, $end=0)
 	  	$tweets = getAPI($url, $params, $headers);
 
 			$lastDate = -1;
-			if(isset($tweets->errors)) continue;
-	  	//if(isset($tweets->errors)) error($tweets->errors, "getTweetsByDate");
-	  	//if(isset($tweets->error)) error($tweets->error, "getTweetsByDate");
+			if(isset($tweets->errors)) continue; //Skip twitter calls that result in an error
+
+			var_dump($tweets);
 
 	  	$len = count($tweets);
 
