20f51e2919bda469b0daf11bb2ac7aabc82e2200
drewmcd621 <mcdermotta@carnegiemuseums.org>
2015-10-28 16:31:10 -0400

    Testing

--- report/unifiedReport.php
+++ report/unifiedReport.php
@@ -4,7 +4,7 @@ require_once "../app/utils/analytics.php";
 
   $unifiedTracker = "ga:105579677";
 
-  $otherTrackers = ["ga:72605316","ga:53193816", "ga:19917087", "ga:30663551"]
+  $otherTrackers = ["ga:72605316","ga:53193816", "ga:19917087", "ga:30663551"];
 
 
 
